202e7d64be781239c24bd673166f1a74409726b2
932824098@qq.com <Bmy932824098>
2021-01-28 18:12:52 +0800

    错误修改

--- src/test/java/fastcache2/Test2.java
+++ src/test/java/fastcache2/Test2.java
@@ -163,7 +163,7 @@ public class Test2 {
 
 	}
 
-`	public static void main(String[] args) {
+	public static void main(String[] args) {
 		CglibProxyFactory.init("fastcache2.dao");
 		new Test2().init();
 	}
